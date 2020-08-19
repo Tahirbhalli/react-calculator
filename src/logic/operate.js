d42c752b3fa6c00bdf375aa3c7f419a790a2270d
tahirbhalli <tahirbhalli16@gmail.com>
2020-08-19 20:22:28 +0500

    operations

---
+++ src/logic/operate.js
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import Big from 'big.js';
+
+const operate = (num1, num2, operation) => {
+  const First = Big(num1);
+  const Second = Big(num2);
+
+  switch (operation) {
+    case '+':
+      return First.plus(Second);
+    case '-':
+      return First.minus(Second);
+    case 'x':
+      return First.times(Second);
+    case '÷':
+      if (Second === '0') {
+        return 'undefined';
+      }
+      return First.div(Second);
+    default:
+      return First.times(Second.times(0.01));
+  }
+};
+
+export default operate;
